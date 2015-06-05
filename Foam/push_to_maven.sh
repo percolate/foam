aa63da3454d6cebabecc440e7442ca968f4a41c9
Brent Watson <brent@brentwatson.com>
2015-06-05 13:11:42 -0400

    Added ability to build and uploading to maven (sonatype).

---
+++ Foam/push_to_maven.sh
@@ -0,0 +1,7 @@
+# To get Maven working, made extensive use of:
+# https://github.com/chrisbanes/gradle-mvn-push (with a few modifications), and 
+# http://zserge.com/blog/gradle-maven-publish.html
+#
+
+./gradlew clean build uploadArchives
+
